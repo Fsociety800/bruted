769a4d0ececd4860098ea4f84b2f79cfd8d4381a
root <root@localhost.localdomain>
2018-05-19 22:33:43 -0300

    BRUTE FORCE IN DIRECTORY WEB

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir /usr/share/bruted/
+cp tools/wordlist.txt /usr/share/bruted/
+cp tools/bruted /bin/
+cp tools/bruted_v /bin/
---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm /usr/share/bruted/wordlist.txt
+rm -r /usr/share/bruted
+rm /bin/bruted
+rm /bin/bruted_v
